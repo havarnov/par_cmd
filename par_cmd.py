b63b58d533e557d47ddf027299f83478112c6b67
Havar Novik <havar.novik@gmail.com>
2010-09-22 12:43:32 +0200

    Rewrote README, cleaned the code and improved the chg_status function

--- par_cmd.py
+++ par_cmd.py
@@ -1,27 +1,32 @@
 #/usr/bin/env python
 
-import parallel, time, os, sys, getopt, optparse
+import parallel, time, os, sys, optparse
 
 class par_cmd:
 
     def status(self):
+        '''This function will return if the pin13 has value True or False'''
         par = parallel.Parallel()
         value = par.getInSelected()
 	return value
 
     def ex_cmd(self, cmd):
-        # denne funksjonen kan byttes ut med 'subprocesses'
+        '''This function will execute the command it's taking as an argument.'''
         os.popen(cmd)
 
     def chg_status(self, command):
+        '''This is the actual "watch pin13" function.'''
         x = 1
         while x == 1:
-            t = time.time()
             st_chg = False
+            t = time.time()
             while time.time() <= t + 2:
                 if self.status() == False:
+                    while self.status() == False:
+                        pass
                     st_chg = True
-                time.sleep(0.1)
+                else:
+                    t = time.time()
             if st_chg == True:
                 self.ex_cmd(command)
 
